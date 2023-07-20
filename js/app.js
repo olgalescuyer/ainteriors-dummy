75bbbb032363f4d64cb88ce4e437290032cb550a
olga <olga.lescuyer@gmail.com>
2023-07-20 16:55:17 +0200

    add: animations & refactoring

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 // countdown timer management system:
 const countDownDate = new Date('Oct 1, 2023 15:00:00').getTime();
 
-// console.log(countDownDate);
-
 const update = setInterval(() => {
   const now = new Date().getTime();
 
@@ -26,23 +24,59 @@ const update = setInterval(() => {
 }, 1000);
 
 // modal management system:
-
 const modal = document.getElementById('modal');
-console.log(modal);
-
 const modalOpenBtn = document.getElementById('modal-open');
-
 const modalCloseBtn = document.getElementById('modal-close');
+
+const links = document.querySelectorAll('ul.modal__links > li');
+const content = document.querySelector('.content-wrapper');
+
+// --aria attributes mangagement system:
+const changeAttributes = () => {
+  //--- to change the button aria attrubute:
+  let value = document
+    .querySelector('[aria-expanded]')
+    ?.getAttribute('aria-expanded');
+  value === 'true' ? (value = 'false') : (value = 'true');
+  document
+    .querySelector('[aria-expanded]')
+    .setAttribute('aria-expanded', value);
+
+  // ---to change the modal aria attrubute:
+  let valueModal = document.querySelector('#modal').getAttribute('aria-hidden');
+  valueModal === 'true' ? (valueModal = 'false') : (valueModal = 'true');
+  document.querySelector('#modal').setAttribute('aria-hidden', valueModal);
+};
+
+const open = (e) => {
+  e.preventDefault();
+  modal.classList.add('modal--open');
+
+  content.classList.add('hide');
+
+  links.forEach((el) => {
+    el.classList.add('show');
+  });
+
+  changeAttributes();
+};
+
+const close = (e) => {
+  e.preventDefault();
+  modal.classList.remove('modal--open');
+
+  content.classList.remove('hide');
+  content.classList.add('show');
+  setTimeout(() => content.classList.remove('show'), 2000);
+
+  links.forEach((el) => {
+    el.classList.remove('show');
+    el.classList.add('hide');
+    setTimeout(() => el.classList.remove('hide'), 2000);
+  });
+
+  changeAttributes();
+};
 
 modalOpenBtn.addEventListener('click', open);
 modalCloseBtn.addEventListener('click', close);
-
-function open(e) {
-  e.preventDefault();
-  modal.classList.add('modal--open');
-}
-
-function close(e) {
-  e.preventDefault();
-  modal.classList.remove('modal--open');
-}
