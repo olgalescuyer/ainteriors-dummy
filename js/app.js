87bab1a74cdcec75d81d631c3287c77f787cdd17
olga <olga.lescuyer@gmail.com>
2023-07-20 21:20:38 +0200

    add

--- js/app.js
+++ js/app.js
@@ -80,3 +80,8 @@ const close = (e) => {
 
 modalOpenBtn.addEventListener('click', open);
 modalCloseBtn.addEventListener('click', close);
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
