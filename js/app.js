0309583ab13fb7986c644d85e824b77636ea4d10
olga <olga.lescuyer@gmail.com>
2023-07-20 08:40:24 +0200

    rewriting to es6

--- js/app.js
+++ js/app.js
@@ -14,8 +14,9 @@ const update = setInterval(() => {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById('timer').innerHTML =
-    days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+  document.getElementById(
+    'timer'
+  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   if (distance < 0) {
     clearInterval(update);
